6b87950d8f20b35ab25baf42320b0d22fe8bface
felipeGsousa <felipe31galdino@gmail.com>
2021-08-28 10:55:16 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
-import { LayoutModule } from './layout/layout.module';
+import {LayoutModule} from './layout/layout.module';
 
 @NgModule({
   declarations: [
